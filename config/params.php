2a0fd8c2891327138b0602e7a2573fc1a408ee9b
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-10-12 07:42:21 -0300

    Add `config` for `yiisoft/config`. (#7)

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // debug
+    'yii.debug' => false, // enable debug mode
+    'yii.debug.allowedIPs' => ['127.0.0.1', '::1'], // allow debug on localhost only
+];
---
+++ config/web/debug.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @var array $params
+ */
+$debug = [];
+
+if (isset($params['yii.debug']) && $params['yii.debug'] === true) {
+    $debug = [
+        // configuration adjustments for 'dev' environment
+        'modules' => [
+            'debug' => [
+                'class' => \yii\debug\Module::class,
+                'allowedIPs' => $params['yii.debug.allowedIPs'] ?? [],
+            ],
+        ],
+    ];
+}
+
+return $debug;
