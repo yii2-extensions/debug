f95f6aef86f2a05d6082b39e50c3d2fea96ab774
Wilmer Arambula <terabytesoftw@gmail.com>
2026-06-21 10:00:34 -0400

    Fix ECS ci.

--- ecs.php
+++ ecs.php
@@ -2,12 +2,22 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\Whitespace\HeredocIndentationFixer;
+use Symplify\CodingStandard\Fixer\Commenting\RemoveDeadVarThisFixer;
+
 /** @var \Symplify\EasyCodingStandard\Configuration\ECSConfigBuilder $builder */
 $builder = require __DIR__ . '/vendor/php-forge/coding-standard/src/ecs-83.php';
 
-return $builder->withPaths(
-    [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ],
-);
+return $builder
+    ->withPaths(
+        [
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    )
+    ->withSkip(
+        [
+            HeredocIndentationFixer::class,
+            RemoveDeadVarThisFixer::class => [__DIR__ . '/src/views'],
+        ],
+    );
